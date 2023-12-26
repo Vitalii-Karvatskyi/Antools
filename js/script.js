e0f0122d9c2d801a05de86fb0e5e43ecd0f1573a
Vitalii <witalik.karwack@gmail.com>
2023-12-26 12:25:21 -0800

    Antools

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.slider-items').slick({
+    infinite: true,
+    slidesToShow: 1,
+    arrows: true,
+    slidesToScroll: 1,
+    prevArrow: $('.arrow-back'),
+    nextArrow: $('.arrow-next')
+  });
